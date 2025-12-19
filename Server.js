653f1055936789788dc8c213d209f78df293225b
SamiTahir09 <samitahir858@gmail.com>
2025-12-19 03:52:33 -0800

    nip

--- Server.js
+++ Server.js
@@ -3,19 +3,27 @@ import userRoutes from './Routes/userRoutes.js';
 import { connectDB } from './Utils/Db.js';
 import cors from 'cors';
 import dotenv from "dotenv";
-dotenv.config();
 
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 connectDB();
 
+
 app.use(express.json());
-app.use(cors())
 
 
-app.use('/api/user', userRoutes)
+app.use(cors({
+    origin: "https://self-test-frontend.vercel.app",
+    credentials: true
+}));
+
+
+app.use('/api/user', userRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
