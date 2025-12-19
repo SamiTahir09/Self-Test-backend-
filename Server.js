92c59dd75e08a2b1a9b38a244317351335dd0971
SamiTahir09 <samitahir858@gmail.com>
2025-12-19 04:10:25 -0800

    nip

--- Server.js
+++ Server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin: "https://self-test-frontend.vercel.app",
+    origin: "https://self-test-frontend-dv51.vercel.app/",
     credentials: true
 }));
 
