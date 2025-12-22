bfd7b99d4f4bf67adee5b329b13af1202db7645c
SamiTahir09 <samitahir858@gmail.com>
2025-12-22 05:29:27 -0800

    nip

--- Server.js
+++ Server.js
@@ -16,11 +16,7 @@ connectDB();
 app.use(express.json());
 
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 
 app.use('/api/user', userRoutes);
